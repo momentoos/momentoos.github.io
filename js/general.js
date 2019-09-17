332bbfacd070da94f19cde80e46ca87592298186
momentoos <46282289+momentoos@users.noreply.github.com>
2019-09-17 15:14:33 -0400

    momentoos 1st
    first-draft

---
+++ js/general.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+$(document).ready(function() {
+  document.getElementsByTagName("html")[0].style.visibility = "visible";
+});
+
+
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeIn(900);
+
+});
